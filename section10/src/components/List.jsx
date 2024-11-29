126b07e64721402e847142f1e48625c413f34c32
Dev SeongMin <hsm990423@gmail.com>
2024-11-29 21:59:39 +0900

    FEAT 리엑트 FrontEnd 최적화

---
+++ section10/src/components/List.jsx
@@ -0,0 +1,46 @@
+import { useMemo, useState } from 'react';
+import '../css/List.css';
+import TodoItem from './TodoItem';
+
+export const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredTodos = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()));
+  };
+
+  const filteredTodos = getFilteredTodos();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log('getAnalyzedData');
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>not done : {notDoneCount}</div>
+      </div>
+      <input onChange={onChangeSearch} type="text" placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete} />;
+        })}
+      </div>
+    </div>
+  );
+};
